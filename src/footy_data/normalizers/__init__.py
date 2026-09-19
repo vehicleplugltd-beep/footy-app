90a416fe3bab5552cf9ac4f8ac28a52accc17607
vehicleplugltd-beep <vehicleplugltd@gmail.com>
2026-09-19 20:53:43 +0100

    Export Understat match normalizer

--- src/footy_data/normalizers/__init__.py
+++ src/footy_data/normalizers/__init__.py
@@ -1,3 +1,3 @@
-from .understat import normalise_understat
+from .understat import normalise_understat, normalise_understat_matches
 
-__all__ = ["normalise_understat"]
+__all__ = ["normalise_understat", "normalise_understat_matches"]
