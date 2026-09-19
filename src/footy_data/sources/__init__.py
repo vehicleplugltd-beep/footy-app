1c3ae12d71c521de70e645b9c07fa25dc5c8fda0
vehicleplugltd-beep <vehicleplugltd@gmail.com>
2026-09-19 20:32:27 +0100

    Add src/footy_data/sources/__init__.py

---
+++ src/footy_data/sources/__init__.py
@@ -0,0 +1,11 @@
+from .soccerdata_source import SoccerDataSource
+from .statsbomb_open import StatsBombOpenSource
+from .football_data_csv import FootballDataCSVSource
+from .openfootball import OpenFootballSource
+
+__all__ = [
+    "SoccerDataSource",
+    "StatsBombOpenSource",
+    "FootballDataCSVSource",
+    "OpenFootballSource",
+]
