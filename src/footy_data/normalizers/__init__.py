19e19bd688bf790ed40080d2fcdd91e0e756c99f
vehicleplugltd-beep <vehicleplugltd@gmail.com>
2026-09-22 07:02:13 +0100

    Export FotMob normalizer

--- src/footy_data/normalizers/__init__.py
+++ src/footy_data/normalizers/__init__.py
@@ -1,3 +1,8 @@
+from .fotmob import normalise_fotmob_match
 from .understat import normalise_understat, normalise_understat_matches
 
-__all__ = ["normalise_understat", "normalise_understat_matches"]
+__all__ = [
+    "normalise_fotmob_match",
+    "normalise_understat",
+    "normalise_understat_matches",
+]
